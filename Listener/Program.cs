715f3c0c18f4240e83a9be26a7b08bceb049547f
Hamza Jeljeli <jeljelihamza@gmail.com>
2019-09-25 13:10:54 +0100

    first commit

---
+++ Listener/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listener
+{
+    class Program
+    {
+        private static string topic = "test";
+        private static string subscription = "hamzajeljeli";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-----   This is a listener   -----");
+            Console.WriteLine(" ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            var serviceBusConnectionString = ConfigurationManager.AppSettings["serviceBus"];
+            var managementClient = new ManagementClient(serviceBusConnectionString);
+            /**
+             * Check if the subscription exist or not
+             * if it doesn't, it will create it.
+             * Else IT WONT WORK.
+             */
+            if (!(managementClient.SubscriptionExistsAsync(topic, subscription).GetAwaiter().GetResult()))
+            {
+                managementClient.CreateSubscriptionAsync(new SubscriptionDescription(topic, subscription)).GetAwaiter();
+            }
+            var subscriptionClient = new SubscriptionClient(serviceBusConnectionString, topic, subscription);
+            subscriptionClient.RegisterMessageHandler(async (msg, cancelationToken) =>
+            {
+                var body = Encoding.UTF8.GetString(msg.Body);
+                Console.WriteLine(body);
+
+                await Task.CompletedTask;
+            },
+            async exception =>
+            {
+                await Task.CompletedTask;
+                Console.WriteLine("Error occured, " + exception.Exception.Message);
+                Console.Beep();
+            }
+            );
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        private static string topic = "test";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("-----   This is a publisher   -----");
+            Console.WriteLine(" ");
+            Console.WriteLine("Hint : Type 'q' to exit.");
+            Console.WriteLine(" ");
+            var loop = true;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("Write you message : ");
+                var message = Console.ReadLine();
+                if (message == "q")
+                    break;
+                var serviceBusConnectionString = ConfigurationManager.AppSettings["serviceBus"];
+                var topicClient = new TopicClient(serviceBusConnectionString, topic);
+                var body = Encoding.UTF8.GetBytes(message);
+                var busMessage = new Message(body);
+                topicClient.SendAsync(busMessage).GetAwaiter().GetResult();
+            } while (loop);
+        }
+    }
+}
